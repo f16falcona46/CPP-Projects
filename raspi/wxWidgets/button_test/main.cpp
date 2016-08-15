ec239efc5d6437b1f07c5f978e092fdbce3f0f6e
Jason Li <li07.jason@gmail.com>
2016-08-14 23:02:47 -0400

    wxtest now with timers

--- raspi/wxWidgets/button_test/main.cpp
+++ raspi/wxWidgets/button_test/main.cpp
@@ -6,8 +6,12 @@ public:
 	MainWindow(const wxString& title, const wxPoint& pos, const wxSize& size);
 
 private:
+	int count;
+	wxStaticText* counterLabel;
+	wxSizer* sizer;
 	void OnButtonClick(wxCommandEvent& event);
-	
+	void OnTimerTimeout(wxTimerEvent& event);
+
 	wxDECLARE_EVENT_TABLE();
 };
 
@@ -17,12 +21,13 @@ public:
 	virtual bool OnInit() override;
 };
 
-enum MainWindowControls
+enum MainWindowIDs
 {
 	ID_Button1 = 1,
 	ID_Button2,
 	ID_Button3,
-	ID_Button4
+	ID_Button4,
+	ID_Timer1
 };
 
 wxBEGIN_EVENT_TABLE(MainWindow, wxFrame)
@@ -30,6 +35,7 @@ wxBEGIN_EVENT_TABLE(MainWindow, wxFrame)
 	EVT_BUTTON(ID_Button2, MainWindow::OnButtonClick)
 	EVT_BUTTON(ID_Button3, MainWindow::OnButtonClick)
 	EVT_BUTTON(ID_Button4, MainWindow::OnButtonClick)
+	EVT_TIMER(ID_Timer1, MainWindow::OnTimerTimeout)
 wxEND_EVENT_TABLE()
 
 wxIMPLEMENT_APP(ButtonTestApp);
@@ -43,9 +49,12 @@ bool ButtonTestApp::OnInit()
 }
 
 MainWindow::MainWindow(const wxString& title, const wxPoint& pos, const wxSize& size) :
-	wxFrame(NULL, wxID_ANY, title, pos, size)
+	wxFrame(NULL, wxID_ANY, title, pos, size),
+	count(0)
 {
 	//wxPanel* panel = new wxPanel(this);
+	wxTimer* timer = new wxTimer(this, ID_Timer1);
+
 	wxGridBagSizer* sizer = new wxGridBagSizer();
 
 	sizer->Add(new wxButton(this, ID_Button1, "Test1!!!", wxDefaultPosition, wxDefaultSize, 0), wxGBPosition(0, 0), wxGBSpan(), wxALL | wxEXPAND, 5);
@@ -53,12 +62,17 @@ MainWindow::MainWindow(const wxString& title, const wxPoint& pos, const wxSize& 
 	sizer->Add(new wxButton(this, ID_Button3, "Test3!!!", wxDefaultPosition, wxDefaultSize, 0), wxGBPosition(1, 0), wxGBSpan(), wxALL | wxEXPAND, 5);
 	sizer->Add(new wxButton(this, ID_Button4, "Test4!!!", wxDefaultPosition, wxDefaultSize, 0), wxGBPosition(1, 1), wxGBSpan(), wxALL | wxALIGN_CENTER, 5);
 
+	this->counterLabel = new wxStaticText(this, -1, "0");
+	sizer->Add(this->counterLabel, wxGBPosition(0, 2), wxGBSpan(), wxALIGN_CENTER, 5);
+
 	sizer->AddGrowableRow(0);
 	sizer->AddGrowableRow(1);
 	sizer->AddGrowableCol(0);
 	sizer->AddGrowableCol(1);
 
 	this->SetSizer(sizer);
+	this->sizer = sizer;
+	timer->Start(1000);
 }
 
 void MainWindow::OnButtonClick(wxCommandEvent& event)
@@ -72,4 +86,11 @@ void MainWindow::OnButtonClick(wxCommandEvent& event)
 	default: text = "The button is unknown!"; break;
 	}
 	wxMessageBox(text.c_str(), "Test title", wxOK | wxICON_INFORMATION);
+}
+
+void MainWindow::OnTimerTimeout(wxTimerEvent& event)
+{
+	++this->count;
+	this->counterLabel->SetLabel(std::to_string(this->count));
+	sizer->Layout();
 }
