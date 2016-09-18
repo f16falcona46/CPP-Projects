a438d65628e373740dd7f633c9b714174be1ddc1
Jason Li <li07.jason@gmail.com>
2016-09-18 00:50:04 -0400

    implemented some functions, but the primary layout function is not done yet

--- win32/LayoutManagerTest/GBLayoutMgr.c
+++ win32/LayoutManagerTest/GBLayoutMgr.c
@@ -38,15 +38,54 @@ void GBLayoutDestroy(GBLayout* layout)
 
 BOOL GBLayoutPerform(HWND parent, const GBLayout* layout)
 {
-
+	//incomplete
+	RECT size;
+	GetClientRect(parent, &size);
+	int totalRows = 0;
+	int totalCols = 0;
+	for (int i = 0; i < layout->numCells; ++i) {
+		
+	}
+	float* totalsWeightsX = HeapAlloc(GetProcessHeap(), 0, sizeof(float) * totalCols);
+	float* totalsWeightY = HeapAlloc(GetProcessHeap(), 0, sizeof(float) * totalCols);
 }
 
 BOOL GBLayoutAddCell(GBLayout* layout, const GBLayoutCell* cell)
 {
+	if (layout->numCells >= layout->capacity) {
+		GBLayoutCell* ptr = HeapReAlloc(GetProcessHeap(), 0, layout->cells, sizeof(GBLayoutCell) * layout->capacity * 2);
+		if (ptr == NULL) return FALSE;
+		else {
+			layout->capacity = layout->capacity * 2;
+			layout->cells = ptr;
+		}
+	}
+	layout->cells[layout->numCells] = *cell;
+	++layout->numCells;
+	return TRUE;
+}
 
+BOOL GBLayoutRemoveCell(GBLayout* layout, int x, int y)
+{
+	for (int i = 0; i < layout->numCells; ++i) {
+		if (layout->cells[i].x == x && layout->cells[i].y == y) {
+			for (int j = i + 1; j < layout->numCells; ++j) {
+				layout->cells[j - 1] = layout->cells[j];
+			}
+			--layout->numCells;
+			return TRUE;
+		}
+	}
+	return FALSE;
 }
 
 BOOL GBLayoutGetCell(const GBLayout* layout, GBLayoutCell* cell, int x, int y)
 {
-
+	for (int i = 0; i < layout->numCells; ++i) {
+		if (layout->cells[i].x == x && layout->cells[i].y == y) {
+			*cell = layout->cells[i];
+			return TRUE;
+		}
+	}
+	return FALSE;
 }
--- win32/LayoutManagerTest/GBLayoutMgr.h
+++ win32/LayoutManagerTest/GBLayoutMgr.h
@@ -11,9 +11,25 @@ typedef struct GBLayoutCell_ {
 	FLOAT weighty;
 	INT preferredx;
 	INT preferredy;
+	UINT align;
+	UINT fill;
 } GBLayoutCell;
 
 struct GBLayout_;
+
+enum GBAlign {
+	GBAlign_CENTER = 0x00,
+	GBAlign_LEFT = 0x01,
+	GBAlign_RIGHT = 0x02,
+	GBAlign_TOP = 0x04,
+	GBAlign_BOTTOM = 0x08
+};
+
+enum GBFill {
+	GBFill_NONE = 0x00,
+	GBFill_VERT = 0x01,
+	GBFill_HORIZ = 0x02
+};
 
 typedef struct GBLayout_ GBLayout;
 
@@ -22,4 +38,5 @@ GBLayout* GBLayoutCreate();
 void GBLayoutDestroy(GBLayout* layout);
 BOOL GBLayoutPerform(HWND parent, const GBLayout* layout);
 BOOL GBLayoutAddCell(GBLayout* layout, const GBLayoutCell* cell);
+BOOL GBLayoutRemoveCell(GBLayout* layout, int x, int y);
 BOOL GBLayoutGetCell(const GBLayout* layout, GBLayoutCell* cell, int x, int y);
