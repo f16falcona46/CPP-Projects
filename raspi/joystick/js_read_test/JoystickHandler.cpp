a3e577266566ac42647324a827d30e53bd568a2f
Jason Li <li07.jason@gmail.com>
2016-09-22 18:56:53 -0400

    still need to finish opening the file descriptor

--- raspi/joystick/js_read_test/JoystickHandler.cpp
+++ raspi/joystick/js_read_test/JoystickHandler.cpp
@@ -1,12 +1,21 @@
 #include "JoystickHandler.h"
+#include <exception>
+#include <unistd.h>
+#include <linux/joystick.h>
 
-JoystickHandler::JoystickHandler()
+JoystickHandler::JoystickHandler() : isSetUp(false)
 {
 }
 
-JoystickHandler::JoystickHandler(const char* joystick)
+JoystickHandler::JoystickHandler(const char* joystick) : isSetUp(false)
 {
 	ChangeJoystick(joystick);
+}
+
+JoystickHandler::~JoystickHandler()
+{
+	StopUpdateThread();
+	//TODO: release fd here...
 }
 
 void JoystickHandler::ChangeJoystick(const char* joystick)
@@ -16,40 +25,77 @@ void JoystickHandler::ChangeJoystick(const char* joystick)
 	std::lock_guard<std::mutex> joystick_guard(joystick_mutex, std::adopt_lock);
 	this->joystick = joystick;
 	//TODO: open fd here
+	isSetUp = true;
 }
 
 std::string JoystickHandler::GetJoystick() const
 {
 	std::lock_guard<std::mutex> guard(joystick_mutex);
+	if (!isSetUp) throw std::logic_error("JoystickHandler wasn't set up.");
 	return this->joystick;
 }
 
 int JoystickHandler::GetNumAxes() const
 {
 	std::lock_guard<std::mutex> guard(axes_mutex);
+	if (!isSetUp) throw std::logic_error("JoystickHandler wasn't set up.");
 	return axes.size();
 }
 
 int JoystickHandler::GetNumButtons() const
 {
 	std::lock_guard<std::mutex> guard(buttons_mutex);
+	if (!isSetUp) throw std::logic_error("JoystickHandler wasn't set up.");
 	return buttons.size();
 }
 
 bool JoystickHandler::GetButtonState(int button) const
 {
 	std::lock_guard<std::mutex> guard(buttons_mutex);
+	if (!isSetUp) throw std::logic_error("JoystickHandler wasn't set up.");
 	return buttons[button];
 }
 
 int16_t JoystickHandler::GetAxisState(int axis) const
 {
 	std::lock_guard<std::mutex> guard(axes_mutex);
+	if (!isSetUp) throw std::logic_error("JoystickHandler wasn't set up.");
 	return axes[axis];
 }
 
 void JoystickHandler::Update()
 {
 	std::lock_guard<std::mutex> guard(fd_mutex);
+	js_event e;
+	int result = read(fd, &e, sizeof(e));
+	if (result != sizeof(e)) throw std::runtime_error("Reading from joystick failed.");
+	if ((e.type & ~JS_EVENT_INIT) == JS_EVENT_BUTTON) {
+		std::lock_guard<std::mutex> buttons_guard(buttons_mutex);
+		buttons[e.number] = e.value;
+	}
+	else if ((e.type & ~JS_EVENT_INIT) == JS_EVENT_AXIS) {
+		std::lock_guard<std::mutex> axes_guard(axes_mutex);
+		axes[e.number] = e.value;
+	}
+}
+
+void JoystickHandler::UpdateLoop()
+{
+	while (runUpdateThread) {
+		Update();
+	}
+}
+
+void JoystickHandler::StartUpdateThread()
+{
+	if (!isSetUp) throw std::logic_error("JoystickHandler wasn't set up.");
+	runUpdateThread = true;
 	//update stuff here
+	updateThread = std::thread(&JoystickHandler::UpdateLoop, this);
+}
+
+void JoystickHandler::StopUpdateThread()
+{
+	runUpdateThread = false;
+	updateThread.join();
 }
--- raspi/joystick/js_read_test/JoystickHandler.h
+++ raspi/joystick/js_read_test/JoystickHandler.h
@@ -5,19 +5,26 @@
 #include <vector>
 #include <mutex>
 #include <string>
+#include <atomic>
+#include <thread>
 
 class JoystickHandler {
 public:
 	JoystickHandler();
 	JoystickHandler(const char* joystick);
+	~JoystickHandler();
 	void ChangeJoystick(const char* joystick);
 	std::string GetJoystick() const;
 	int GetNumAxes() const;
 	int GetNumButtons() const;
 	bool GetButtonState(int button) const;
 	int16_t GetAxisState(int axis) const;
-	void Update();
+	void StartUpdateThread();
+	void StopUpdateThread();
 private:
+	void Update();
+	void UpdateLoop();
+
 	std::vector<bool> buttons;
 	mutable std::mutex buttons_mutex;
 	std::vector<int16_t> axes;
@@ -25,7 +32,10 @@ private:
 	int fd;
 	mutable std::mutex fd_mutex;
 	std::string joystick;
-	mutable std::string joystick_mutex;
+	mutable std::mutex joystick_mutex;
+	std::atomic<bool> isSetUp;
+	std::atomic<bool> runUpdateThread;
+	std::thread updateThread;
 };
 
 #endif //JOYSTICK_HANDLER_H
