40062e6bf78c23da58b8624a91576132ac95bf9e
Jason Li <li07.jason@gmail.com>
2016-09-22 22:49:21 -0400

    finished tester for joystick

--- raspi/joystick/js_read_test/main.cpp
+++ raspi/joystick/js_read_test/main.cpp
@@ -1,7 +1,20 @@
+#include <unistd.h>
+#include <iostream>
 #include "JoystickHandler.h"
 
 int main()
 {
 	JoystickHandler handler("/dev/input/js0");
+	handler.StartUpdateThread();
+	while (true) {
+		for (int i = 0; i < handler.GetNumButtons(); ++i) {
+			std::cout << "Button " << i << ": " << handler.GetButtonState(i) << '\n';
+		}
+		for (int i = 0; i < handler.GetNumAxes(); ++i) {
+			std::cout << "Axis " << i << ": " << handler.GetAxisState(i) << '\n';
+		}
+		std::cout << "\n\n";
+		sleep(1);
+	}
 	return 0;
 }
