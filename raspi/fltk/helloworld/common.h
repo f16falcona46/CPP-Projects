5540ad5d0bd28a64b42878fa517b58fe4cae91be
Jason <li07.jason@gmail.com>
2016-10-10 01:47:39 -0400

    added fltk stuffs

---
+++ raspi/fltk/helloworld/common.h
@@ -0,0 +1,11 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Group.H>
+#include <FL/Fl_Button.H>
+#include <FL/Enumerations.H>
+
+#endif //COMMON_H
---
+++ raspi/fltk/helloworld/precompiled_headers.h
@@ -0,0 +1,9 @@
+#ifndef PRECOMPILED_HEADERS_H
+#define PRECOMPILED_HEADERS_H
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <cmath>
+
+#endif //PRECOMPILED_HEADERS_H
