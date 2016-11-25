882234ae00e1b1c3aafbd86f88f48b3fce2cdd99
Jason Li <li07.jason@gmail.com>
2016-11-25 00:31:10 -0500

    testing radio buttons

--- raspi/fltk/helloworld/common.h
+++ raspi/fltk/helloworld/common.h
@@ -2,10 +2,11 @@
 #define COMMON_H
 
 #include <FL/Fl.H>
-#include <FL/Fl_Window.H>
+#include <FL/Fl_Double_Window.H>
 #include <FL/Fl_Box.H>
 #include <FL/Fl_Group.H>
 #include <FL/Fl_Button.H>
+#include <FL/Fl_Radio_Round_Button.H>
 #include <FL/Enumerations.H>
 
 #endif //COMMON_H
