6c835d329024cfe7d7c938a5ea4bc50cab6ca832
Jason Li <li07.jason@gmail.com>
2017-06-20 12:13:38 +0800

    Now does a convolution, I think

--- win32/OpenClTest2/OpenClTest2/Main.cpp
+++ win32/OpenClTest2/OpenClTest2/Main.cpp
@@ -1,13 +1,17 @@
 #include <cstdlib>
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <Windows.h>
 #include <vector>
 #include <sstream>
 #include <locale>
 #include <codecvt>
+#include <stdexcept>
+#define __CL_ENABLE_EXCEPTIONS
 #include <CL/cl.hpp>	
 
+/*
 BOOL check_error(cl_int err, const std::wstring& str)
 {
 	if (err != CL_SUCCESS) {
@@ -16,10 +20,20 @@ BOOL check_error(cl_int err, const std::wstring& str)
 	}
 	return FALSE;
 }
+*/
+
+std::string ReadString(const std::string& file)
+{
+	std::ifstream is(file, std::ifstream::in | std::ifstream::binary);
+	std::stringstream ss;
+	ss << is.rdbuf();
+	return ss.str();
+}
 
 int main()
 {
 	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> utf16conv;
+	/*
 	cl_int err = CL_SUCCESS;
 	std::vector<cl::Platform> platforms;
 	err = cl::Platform::get(&platforms);
@@ -46,8 +60,86 @@ int main()
 		}
 	}
 	std::wcout << ss.str();
-	std::wcout << L"Press ENTER to continue...";
-	std::wstring dummy;
-	std::getline(std::wcin, dummy);
+	*/
+	cl_float in_image[] = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f, 13.0f, 14.0f, 15.0f, 16.0f };
+	cl_float out_image[4];
+	cl_float filter[] = { 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f };
+	cl_float foo[50000] = { -1.0f };
+	std::cout << "out\n";
+	for (int i = 0; i < 4; ++i) {
+		std::cout << out_image[i] << '\n';
+	}
+	try {
+		//set up CL, get device
+		std::vector<cl::Platform> platforms;
+		cl::Platform::get(&platforms);
+		cl::Platform target_platform = platforms[0];
+		for (const cl::Platform& platform : platforms) {
+			if (platform.getInfo<CL_PLATFORM_VENDOR>() == "NVIDIA Corporation") {
+				target_platform = platform;
+				break;
+			}
+		}
+		cl_context_properties cprops[3] = { CL_CONTEXT_PLATFORM, (cl_context_properties)target_platform(), 0 };
+		cl::Context ctx(CL_DEVICE_TYPE_GPU, cprops, nullptr, nullptr);
+		std::vector<cl::Device> devices = ctx.getInfo<CL_CONTEXT_DEVICES>();
+		cl::CommandQueue cqueue(ctx, devices[0]);
+
+		cl::Buffer in_image_buf(ctx, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_float) * 16, in_image);
+		cl::Buffer out_image_buf(ctx, CL_MEM_WRITE_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_float) * 4, out_image);
+		cl::Buffer filter_buf(ctx, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_float) * 9, filter);
+		cl::Buffer foo_buf(ctx, CL_MEM_WRITE_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(cl_float) * 50000, foo);
+
+		//get and build program
+		const std::string& kernel_source = ReadString("kernel.cl");
+		cl::Program::Sources sources(1, std::make_pair(kernel_source.c_str(), kernel_source.length()));
+		cl::Program program(ctx, sources);
+		program.build(devices);
+
+		//get kernel and bind args
+		cl::Kernel kernel(program, "Convolve");
+		kernel.setArg(0, in_image_buf);
+		kernel.setArg(1, filter_buf);
+		kernel.setArg(2, out_image_buf);
+		kernel.setArg(3, 3);
+		kernel.setArg(4, foo_buf);
+		cqueue.finish();
+
+		cl::Event e;
+		cqueue.enqueueNDRangeKernel(kernel, cl::NDRange(), cl::NDRange(2, 2), cl::NDRange(2, 2), 0, &e);
+		e.wait();
+
+		cqueue.finish();
+		
+		cqueue.enqueueReadBuffer(out_image_buf, CL_TRUE, 0, sizeof(cl_float) * 4, out_image);
+		cqueue.enqueueReadBuffer(foo_buf, CL_TRUE, 0, sizeof(cl_float) * 50000, foo);
+		cqueue.finish();
+		std::cout << "out\n";
+		for (int i = 0; i < 4; ++i) {
+			std::cout << out_image[i] << '\n';
+		}
+
+		std::cout << "in\n";
+		for (int i = 0; i < 16; ++i) {
+			std::cout << in_image[i] << '\n';
+		}
+
+		std::cout << "filt\n";
+		for (int i = 0; i < 9; ++i) {
+			std::cout << filter[i] << '\n';
+		}
+
+		std::cout << "foo\n";
+		for (int i = 0; i < 50000; ++i) {
+			if (foo[i]) std::cout << i << '=' << foo[i] << '\n';
+		}
+
+		std::wcout << L"Press ENTER to continue...";
+		std::wstring dummy;
+		std::getline(std::wcin, dummy);
+	}
+	catch (const std::exception& e) {
+		std::cout << e.what() << '\n';
+	}
 	return 0;
 }
--- win32/OpenClTest2/OpenClTest2/kernel.cl
+++ win32/OpenClTest2/OpenClTest2/kernel.cl
@@ -1,3 +1,17 @@
-__kernel void test()
+__kernel void Convolve(const __global float * InImage, __constant float * Filter, __global float * OutImage, const int FilterWidth, __global float * Foo)
 {
+	const int width = get_global_size(0);
+	const int x = get_global_id(0);
+	const int y = get_global_id(1);
+	const int in_width = width + FilterWidth - 1;
+	float sum = 0.0f;
+	for (int y_filt = 0; y_filt < FilterWidth; ++y_filt) {
+		const int y_filt_idx = FilterWidth * y_filt;
+		const int y_in_idx = in_width * (y + y_filt);
+		for (int x_filt = 0; x_filt < FilterWidth; ++x_filt) {
+			sum += Filter[x_filt + y_filt_idx] * InImage[x_filt + x + y_in_idx];
+			Foo[(x_filt + y_filt_idx) + 100 * ((x + 1) * 10 + y + 1)] = x_filt + x + y_in_idx;
+		}
+	}
+	OutImage[x + y * width] = sum;
 }
