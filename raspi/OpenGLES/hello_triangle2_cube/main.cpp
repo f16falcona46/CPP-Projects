e8803807ccff83be9ee99a8551c55f5355d80401
Jason Li <li07.jason@gmail.com>
2018-04-01 23:21:10 -0700

    Many cubes rotate simultaneously.

--- raspi/OpenGLES/hello_triangle2_cube/main.cpp
+++ raspi/OpenGLES/hello_triangle2_cube/main.cpp
@@ -9,26 +9,35 @@
 #include <chrono>
 #include <iostream>
 
-void update_cube_state(const GLES_State* state, ObjectState* cube)
+void update_cube_state(const GLES_State* state, ObjectState* cube, int x, int y, float x_shift, float y_shift, float z_shift)
 {
-	int x, y;
 	const float distance = 20.0f;
 	const float scale = 5.0f;
-	get_mouse(state, &x, &y);
 	cube->angle_x = ((float) x - (float) state->screen_width / 2.0f)
 		/ (float) state->screen_width * scale;
 	cube->angle_y = ((float) y - (float) state->screen_height / 2.0f)
 		/ (float) state->screen_width * scale;
 	cube->Projection = glm::perspective(glm::radians(45.0f),
 		(float) state->screen_width / (float) state->screen_height,
-		0.1f, 30.0f);
+		0.1f, 70.0f);
+	/*
 	cube->View = glm::lookAt(
 		glm::vec3(distance * std::cos(cube->angle_x) * std::cos(cube->angle_y),
 			distance * std::sin(cube->angle_x) * std::cos(cube->angle_y),
 			distance * std::sin(cube->angle_y)),
 		glm::vec3(0, 0, 0),
 		glm::vec3(0, 0, 1));
-	cube->Model = glm::scale(glm::vec3(2.0f, 2.0f, 2.0f));
+	*/
+	cube->View = glm::lookAt(
+		glm::vec3(distance,
+			distance,
+			distance),
+		glm::vec3(0, 0, 0),
+		glm::vec3(0, 0, 1));
+	cube->Model = glm::scale(glm::vec3(1.0f, 1.0f, 1.0f));
+	cube->Model = glm::translate(cube->Model, glm::vec3(x_shift, y_shift, z_shift));
+	cube->Model = glm::rotate(cube->Model, cube->angle_x, glm::vec3(0.0f, 0.0f, 1.0f));
+	cube->Model = glm::rotate(cube->Model, cube->angle_y, glm::vec3(1.0f, 0.0f, 0.0f));
 	cube->MVP = cube->Projection * cube->View * cube->Model;
 }
 
@@ -43,17 +52,26 @@ int main()
 	compile_shaders(&state, &cubedata);
 	init_buffers_cube(&state, &cubedata);
 
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	glBindBuffer(GL_ARRAY_BUFFER, cubedata.vert_buf);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, cubedata.vert_idx_buf);
 	glUseProgram(cubedata.program);
 	while (1) {
-		update_cube_state(&state, &cube);
+		int mouse_x, mouse_y;
+		get_mouse(&state, &mouse_x, &mouse_y);
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
-		glUniformMatrix4fv(cubedata.unif_MVP, 1, GL_FALSE, &cube.MVP[0][0]);
-		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, nullptr);
+		for (float x = -6; x <= 6; x += 3.0) {
+			for (float y = -6; y <= 6; y += 3.0) {
+				for (float z = -6; z <= 6; z += 3.0) {
+					update_cube_state(&state, &cube, mouse_x, mouse_y, x, y, z);
+					glUniformMatrix4fv(cubedata.unif_MVP, 1, GL_FALSE, &cube.MVP[0][0]);
+					glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, nullptr);
+				}
+			}
+		}
 
 		glFlush();
 		glFinish();
